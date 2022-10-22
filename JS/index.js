7e0a1936bfac48aeb000db0f63846b63a089708f
Hannah Daniels <101903080+danielshannahn@users.noreply.github.com>
2022-10-22 12:45:43 -0400

    Import from HW20

---
+++ JS/index.js
@@ -0,0 +1,18 @@
+console.log("your index.js file is loading like dookie!"
+);
+
+console.count("inline");
+console.count("casestudy")
+
+$('.hamburger-lines').click(function() {
+    if($('.ham-menu').is(':visible')) {
+        $('.ham-menu').hide();
+    }else{
+        $('.ham-menu').show();
+    }
+    
+})
+
+$(".hamburger-lines").click(function(){
+    (".inline").slidedown();
+});
